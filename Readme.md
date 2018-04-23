ad9673ed8760550c8c45c7a265d49a901d7b9ca3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create simple bars
+
+
+<p>This example shows how to create two bars with commands. The first bar represents the main menu toolbar, containing the File and Format menus. The second bar is displayed at the left edge of the BarManager. It contains a single Bold check item.</p><p>The bars are docked to the BarContainerControls which are created by default (the CreateStandardLayout option is set to True).</p>
+
+<br/>
+
+
---
+++ VB/CreateBars_Ex/MainPage.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+
+Namespace CreateBars_Ex
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub btnExit_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+
+		End Sub
+
+		Private Sub checkBtnBold_CheckedChanged(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+
+		End Sub
+	End Class
+End Namespace
